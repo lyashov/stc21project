5ddcd766989dd08c29b93f34cd035645e5c209ea
= <=>
2019-11-30 17:17:32 +0300

    + jpa dependency + logger properties

--- src/main/java/ru/innopolis/stc21/MedApplication.java
+++ src/main/java/ru/innopolis/stc21/MedApplication.java
@@ -1,5 +1,6 @@
 package ru.innopolis.stc21;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
