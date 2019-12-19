827d1280b0d33e8daa38568a76f0217666fa358e
Meg6pam <talnah20@yandex.ru>
2019-12-20 00:08:52 +0300

    Почти всё готово. Не могу вытащить геопозицию из метода

--- src/main/java/ru/innopolis/stc21/med/controller/GeoIPController.java
+++ src/main/java/ru/innopolis/stc21/med/controller/GeoIPController.java
@@ -1,28 +1,39 @@
 package ru.innopolis.stc21.med.controller;
 
+import org.gavaghan.geodesy.Ellipsoid;
+import org.gavaghan.geodesy.GeodeticCalculator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ru.innopolis.stc21.med.model.ClinicEntity;
+import ru.innopolis.stc21.med.model.MedicalHistoryEntity;
+import ru.innopolis.stc21.med.service.ClinicService;
 import ru.innopolis.stc21.med.service.GeoIP;
 import ru.innopolis.stc21.med.service.GeoIPService;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 @Controller
 public class GeoIPController {
 
-    public GeoIPController() {
-    }
+    ClinicService clinicService;
 
-   /* @GetMapping("/geoip")
-    public String geoGetIp() {
-        return "geoip";
-    }*/
+    @Autowired
+    public GeoIPController(ClinicService clinicService) {
+        this.clinicService = clinicService;
+    }
 
     @RequestMapping(value = "/geoip", method = {RequestMethod.GET,RequestMethod.POST})
     public String geoIpLocation(
             HttpServletRequest httpServletRequest
     ) throws Exception {
-        String ipAddress = httpServletRequest.getRemoteAddr();
+        //String ipAddress = httpServletRequest.getRemoteAddr();
+        String ipAddress = "188.130.155.153";
 
         System.out.println(ipAddress);
 
@@ -33,7 +44,34 @@ public class GeoIPController {
     }
 
     @GetMapping("/hospitallocation")
-    public String hello() {
+    public String hello(Model model) {
+
+        List<ClinicEntity> clinicEntities = clinicService.getAllClinics();
+        List<ClinicEntity> sortedClinicEntities = clinicsSort(clinicEntities, userGeoPosition);
+        model.addAttribute("clinicEntities", sortedClinicEntities);
         return "hospitallocation";
     }
+
+    private List<ClinicEntity> clinicsSort (List<ClinicEntity> list, GeoIP geo){
+        Map<Double, ClinicEntity> map = new TreeMap<>();
+        List<ClinicEntity> result = new ArrayList<>();
+
+        double geoX = Double.parseDouble(geo.getLatitude());
+        double geoY = Double.parseDouble(geo.getLongitude());
+
+        for (ClinicEntity clinic : list){
+            double clinicX = Double.parseDouble(clinic.getCoord_x());
+            double clinicY = Double.parseDouble(clinic.getCoord_y());
+
+            double distance = Math.sqrt(Math.pow(clinicX - geoX,2) + Math.pow(clinicY - geoY, 2));
+            map.put(distance, clinic);
+        }
+
+        for(Map.Entry<Double, ClinicEntity> entry : map.entrySet()){
+            result.add(entry.getValue());
+        }
+        return result;
+    }
+
+
 }
--- src/main/resources/templates/hospitallocation.ftl
+++ src/main/resources/templates/hospitallocation.ftl
@@ -10,6 +10,30 @@
     <input type="hidden" name="_csrf" value="${_csrf.token}" />
     <input class="btn btn-primary btn-block" type="submit" value= "вернуться к истории!">
 
+    <table class="table">
+        <thead>
+        <tr class="table-secondary">
+            <th scope="col">Расстояние до больницы</th>
+            <th scope="col">Медицинское учреждение</th>
+            <th scope="col">Адрес</th>
+            <th scope="col">Телефон</th>
+            <th scope="col">E-mail</th>
+        </tr>
+        </thead>
+        <tbody>
+        <input type="hidden" name="_csrf" value="${_csrf.token}"/>
+        <#list clinicEntities as hospital>
+            <tr>
+                <td>" "</td>
+                <td>${hospital.name}</td>
+                <td>${hospital.address}</td>
+                <td>${hospital.telephone}</td>
+                <td>${hospital.email}</td>
+            </tr>
+        </#list>
+         </tbody>
+    </table>
+
 </form>
 
 </body>
